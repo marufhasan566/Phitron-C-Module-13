175d750b2887105952568eadbb62d437af30361b
marufhasan566 <marufhasan.mh1@gmail.com>
2023-05-15 11:08:02 +0600

    Sum of Two Elemeny from a array

---
+++ Sum_of_Two.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+int main()
+{
+    int n;
+    scanf("%d", &n);
+    int arr[n];
+    for (int i = 0; i < n; i++)
+    {
+        scanf("%d", &arr[i]);
+    }
+    int sum;
+    scanf("%d", &sum);
+    int flag = 0;
+    for (int i = 0; i < n - 1; i++)
+    {
+        for (int j = i + 1; j < n; j++)
+        {
+            if (arr[i] + arr[j] == sum)
+            {
+                flag = 1;
+            }
+        }
+    }
+    if (flag == 0)
+    {
+        printf("No\n");
+    }
+    if (flag == 1)
+    {
+        printf("Yes\n");
+    }
+
+    return 0;
+}
